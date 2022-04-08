8eee4a733d3b2c542f41f93ab4b11e74ade841fd
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-08 13:21:24 +0530

    ellipse .c

---
+++ c program/Ellipse .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+float pi=3.142;
+int E,a,b;
+printf("Enter a:");
+scanf("%d",&a);
+printf("Enter b:");
+scanf("%d",&b);
+E=pi*a*b;
+printf("Ellipse is=%d",E);
+}
