c35a17471214b15e76661dcd8d7b7c9d964f7342
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-08 12:31:54 +0530

    add .c

---
+++ c program/add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2;
+	int result;
+	
+	printf("enter the num1: ");
+	scanf("%d",&num1);
+	
+	printf("enter the num: ");
+	scanf("%d",&num2);
+
+result= num1 + num2;
+
+printf("add is =%d",result);
+	
+}
