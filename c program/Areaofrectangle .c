b6982dc92bce48544b0beb8caa3b44bdd53db7f0
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-08 12:45:36 +0530

    Areaofrectangle .c

---
+++ c program/Areaofrectangle .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{ 
+int height,width;
+height=7;
+width=5;
+float area,perimeter;
+
+perimeter=2*(height+width);
+printf("perimeter of the rectangle=%f inches\n",perimeter);
+
+area=(height*width);
+printf("area of the rectangle=%f square inches\n",area);
+
+}
