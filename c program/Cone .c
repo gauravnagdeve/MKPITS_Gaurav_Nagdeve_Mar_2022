48f04d62b2cec0b97bab76b52bd023947060060f
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-08 12:54:34 +0530

    cone .c

---
+++ c program/Cone .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float pi=3.142;
+	int c,r,l;
+	printf("Enter r:");
+	scanf("%d",&r);
+	printf("Enter l:");
+	scanf("%d",&l);
+	c=pi*r*l;
+	printf("cone is=%d",c);
+}
