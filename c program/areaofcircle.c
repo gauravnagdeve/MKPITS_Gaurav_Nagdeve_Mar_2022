cb349d674cfdd96ec4fde74420c992623d512a37
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-08 12:35:40 +0530

    area of circle .c

---
+++ c program/areaofcircle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int radius;
+	float pi,perimeter,area;
+	radius=6;
+	pi=3.142;
+	
+	perimeter=2*pi*radius;
+	printf("perimeter of circle=%f inches\n",perimeter);
+	
+	area=pi*radius*radius;
+	printf("area of circle=%f square inches\n",area);
+}
