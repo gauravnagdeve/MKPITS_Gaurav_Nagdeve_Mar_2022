b545c2e87982bdf62c75d39386cc6f908873bd4c
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-08 12:50:56 +0530

    circle .c

---
+++ c program/Circle .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float circle,pi=3.14;
+	int radius;
+	printf("Enter radius:");
+	scanf("%d",&radius);
+	circle=pi*radius*radius;
+	printf("%f",circle);
+}
