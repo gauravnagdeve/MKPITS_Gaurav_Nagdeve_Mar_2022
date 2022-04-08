628773bd694522c165be16d967e8a1b60d020435
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-08 13:19:03 +0530

    divoftwonumbers .c

---
+++ c program/divoftwonumbers .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int num1,num2,result;
+	printf("Enter num1:");
+	scanf("%d",&num1);
+	printf("Enter num2:");
+	scanf("%d",&num2);
+	result=num1-num2;
+	printf("result is=%d",result);
+}
