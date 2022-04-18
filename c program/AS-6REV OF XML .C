85788aa91b9d48143ca490d3d3fc6d20299b71f2
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-18 16:19:15 +0530

    as-6rev of XML .C

---
+++ c program/AS-6REV OF XML .C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char a,b,c;
+	printf("test charactors :'X''M''l' ");
+	a='X';
+	b='M';
+	c='L';
+	printf("the reverse of XML is %c%c%c is %c%c%c\n",
+	a,b,c,
+	c,b,a);
+	
+}
