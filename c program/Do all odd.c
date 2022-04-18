bf51c9f164f1a348dd512887c67ca8d71edfe146
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-04-18 17:15:32 +0530

    do all odd.c

---
+++ c program/Do all odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+	
+{
+	int i=1;
+  printf("Odd num:\n");
+  do{
+  	if(i%2!=0)
+  	{
+  		printf("%d\n",i);
+	  }
+	  i++;
+  }
+  while(i<=10);
+}
