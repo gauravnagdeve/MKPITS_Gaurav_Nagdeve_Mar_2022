573afba9a2b74da6daeccaf193bbe5233bbfbd23
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-05-03 19:32:05 +0530

    assighment 1.cpp

---
+++ c program/Assighnment 1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float x,y,div;
+	printf("Enter the 1st number");
+	scanf("%f",&x);
+	printf("Enter the 2nd number");
+	scanf("%f",&y);
+	if(y!=0)
+
+	{
+		div=x/y;
+		printf("The division of these numbers is %.1f",div);
+	}
+	else
+	{
+		printf("The division of these numbers is NOT possible");
+	}
+}
+
