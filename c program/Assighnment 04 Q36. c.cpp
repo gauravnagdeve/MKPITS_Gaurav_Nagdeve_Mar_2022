9f064b922b931cdc3fdcac04dab16d2739068024
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-05-03 20:09:55 +0530

    Assighnment 04 Q36. c

---
+++ c program/Assighnment 04 Q36. c.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+ {	
+	int n1, n2, temp, i, sum=0;
+    printf("Enter the 1st number "); 
+    scanf("%d", &n1);
+    printf("Enter the 2nd number ");
+    scanf("%d", &n2);
+	i=n1+1;	
+    if(n1>n2) 
+	{
+		temp=n2;
+		n2=n1;
+		n1=temp;
+	}
+
+	while(i<n2) 
+	{
+		if((i%7) == 2 || (i%7) == 3)
+		 {
+			printf("%d\n", i);
+	     }
+	    i++;
+	}
+
+	return 0;
+} 
