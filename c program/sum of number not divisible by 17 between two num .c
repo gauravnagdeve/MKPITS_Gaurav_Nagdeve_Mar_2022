6ea78650bc0fcf0d54ee86764c40a00f4e43e226
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-05-04 15:53:39 +0530

    sum of number not divisible by 17 between two num .c

---
+++ c program/sum of number not divisible by 17 between two num .c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n1,n2,i,sum,temp;
+printf("Enter the 1st number");
+scanf("%d",&n1);
+printf("Enter the 2nd number");
+scanf("%d",&n2);
+if(n1 > n2)
+    {
+		temp = n2;
+		n2 = n1;
+		n1 = temp;
+	}
+
+	for(i=n1;i<= n2;i++)
+	{
+		if((i % 17) != 0) 
+		{
+			sum += i;
+		}
+
+	}
+
+	printf("The sum of all number which is not divisible by 17 is %d\n", sum);
+
+	return 0;
+
+	}
+
