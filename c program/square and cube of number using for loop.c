9e33d78e9b7f34527abf12ba31974fbdd3c9f695
gauravnagdeve <gauravnagdeve563@gmail.com>
2022-05-04 16:24:20 +0530

    square and cube of number using for loop .c

---
+++ c program/square and cube of number using for loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,i,s,c;
+	printf("Enter the number");
+	scanf("%d",&n1);
+	i=1;
+	for(i=1;i<=n1;i++)
+	{
+		s=i*i;
+		c=i*i*i;
+	printf("The number i.e %d it's Square is %d and it's Cube is %d .\n",i,s,c);
+    }
+}   
